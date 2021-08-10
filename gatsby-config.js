857e5f9d834b8556d66989a88b5fcf75c27a98a9
tomel1s <tomsin418@gmail.com>
2021-08-10 22:05:43 +0300

    init

---
+++ gatsby-config.js
@@ -0,0 +1,61 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    "gatsby-plugin-svgr",
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `@slixites/gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Manrope\:400,600,700,800`, `work sans\:400`],
+        display: "swap",
+        preconnect: true,
+        attributes: {
+          rel: "stylesheet preload prefetch",
+          as: "style",
+        },
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-gatsby-cloud`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+         * The full URL of the WordPress site's GraphQL API.
+         * Example : 'https://www.example-site.com/graphql'
+         */
+        url: `http://memby.local/graphql`,
+      },
+    },
+  ],
+}
---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+// const HolyGrailBody = styled.div`
+//   display: flex;
+//   flex: 1;
+//   flex-direction: column;
+// `
+
+// const HolyGrail = styled.div`
+//   display: flex;
+//   min-height: 100vh;
+//   flex-direction: column;
+// `
+export const Layout = ({ children }) => {
+  return (
+    // <HolyGrail>
+    //   <Header />
+    //   <HolyGrailBody>{children}</HolyGrailBody>
+    //   <Footer />
+    // </HolyGrail>
+    <div></div>
+  )
+}
